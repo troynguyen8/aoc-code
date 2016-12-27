6a58294714a0fc48e223b1119bc538d7c18fac81
troynguyen8 <troynguyen8@gmail.com>
2016-12-27 15:09:44 -0600

    Add files via upload

---
+++ Advent Of Code 2015/aoc15-day11/aoc15-day11-prob1.js
@@ -0,0 +1,63 @@
+/**
+ * @author Troy Nguyen
+ * 12/26/16
+ * Advent of Code 2015, Day 11 problem 1
+ */
+ 
+var input = 'cqjxjnds';
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+function isValidPassword(password) {
+	if(password.includes('i') || password.includes('o') || password.includes('l')) {
+		return false;
+	}
+	
+	var containsStraight = false;
+	for(let i = 0; i <= password.length - 3; i++) {
+		if(ALPHABET.includes(password.substring(i, i + 3))) {
+			containsStraight = true;
+			break;
+		}
+	}
+	
+	if(!containsStraight) {
+		return false;
+	}
+	
+	if(password.search(/(\w)\1/) === -1) {
+		return false;
+	}
+	
+	if(password.match(/(\w)\1/g).length < 2) {
+		return false;
+	}
+	
+	return true;
+}
+
+function incrementPassword(password) {
+	var passwordArr = [...password];
+	passwordArr.forEach((a, index) => passwordArr[index] = passwordArr[index].charCodeAt(0));
+	
+	passwordArr[passwordArr.length - 1]++;
+	
+	for(let i = passwordArr.length - 1; i >= 0; i--) {
+		if(passwordArr[i] === 123) {
+			passwordArr[i] = 97;
+			passwordArr[i - 1]++;
+		}
+	}
+	
+	let adjustedPassword = '';
+	for(let i = 0; i < passwordArr.length; i++) {
+		adjustedPassword = adjustedPassword + String.fromCharCode(passwordArr[i]);
+	}
+	
+	return adjustedPassword;
+}
+
+while(!isValidPassword(input)) {
+	input = incrementPassword(input);
+}
+
+console.log(input);
