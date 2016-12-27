a8fbebe0f79fa78a5e8678a3edd80b31a2c63f3c
troynguyen8 <troynguyen8@gmail.com>
2016-12-26 22:28:39 -0600

    Add files via upload

---
+++ Advent Of Code 2015/aoc15-day10/aoc15-day10-prob1.js
@@ -0,0 +1,22 @@
+/**
+ * @author Troy Nguyen
+ * 12/26/16
+ * Advent of Code 2016, Day 10 problem 1
+ */
+ 
+var input = '1113122113';
+
+function lookAndSay(numberString) {
+	let returnString = '';
+	let splitString = numberString.match(/(\d)\1*/g);
+	
+	splitString.forEach(entry => returnString = returnString + entry.length + entry.charAt(0));
+	
+	return returnString;
+}
+
+for(let i = 1; i <= 40; i++) {
+	input = lookAndSay(input);
+}
+
+console.log(input.length);
